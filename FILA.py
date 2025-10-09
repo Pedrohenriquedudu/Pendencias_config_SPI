29abefd0b22a6c97315906327c05f090e734573b
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2025-10-09 09:58:57 -0300

    Update FILA.py

--- FILA.py
+++ FILA.py
@@ -144,7 +144,7 @@ else:
 
         with st.expander(f"{cor_emoji} {tarefa['descricao']}"):
             st.write(f"👨‍🔧 Técnico: {tarefa['nome']}")
-            st.write(f"👨‍🔧 ID_vantive: {tarefa['Id_vantive']}")
+            st.write(f"👨‍🔧 ID_vantive: {tarefa['ID_vantive']}")
             st.write(f"📞 Telefone: {tarefa['telefone']}")
             st.write(f"📅 Criada em: {tarefa['data_criacao']}")
             st.write(f"📍 Status atual: **{tarefa['status']}**")
