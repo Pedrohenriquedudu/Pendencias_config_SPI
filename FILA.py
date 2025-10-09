861002bdeb80900136daacebd158df1f8538df15
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2025-10-08 22:01:04 -0300

    Update FILA.py

--- FILA.py
+++ FILA.py
@@ -3,7 +3,6 @@ import pandas as pd
 import os
 from datetime import datetime
 from io import BytesIO
-import matplotlib as plt
 
 st.set_page_config(page_title="Gestão de Tarefas SPI", layout="wide")
 
