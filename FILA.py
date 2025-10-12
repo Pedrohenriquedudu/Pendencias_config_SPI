4d03319610781dac9cd01cf2d3051c8ab76eaf0a
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2025-10-12 20:44:25 -0300

    Update FILA.py

--- FILA.py
+++ FILA.py
@@ -112,7 +112,7 @@ with st.form("form_tarefa"):
 
 if enviar:
         if id_tarefa and nome and telefone and descricao:
-            usuario_criador = st.session_state["usuario_atual"]
+            usuario_criador = st.session_state["usuario_criador"]
             nova = {
                 "id": id_tarefa,
                 "nome": nome,
