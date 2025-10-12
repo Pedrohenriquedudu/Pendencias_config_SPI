77546b4c62999b580a9a8b9546ba2d67d98835ff
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2025-10-12 20:59:37 -0300

    Update FILA.py

--- FILA.py
+++ FILA.py
@@ -123,9 +123,9 @@ if enviar:
                 "assumido_em": "",
                 "encerrado_por": "",
                 "encerrado_em": ""
-                }
+        }
             st.session_state["tarefas"].append(nova)
-            st.success(f"✅ Tarefa {id_tarefa} adicionada com sucesso!")
+            st.success(f"✅ Tarefa adicionada com sucesso!")
         else:
             st.warning("Por favor, preencha todos os campos.")
 
