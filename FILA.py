fd006e04493ab7cd3fc2f3ba261ce15ab19e484d
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2025-10-13 21:34:27 -0300

    Update FILA.py

--- FILA.py
+++ FILA.py
@@ -104,7 +104,7 @@ tarefas = carregar_tarefas()
 # --------------------------
 st.subheader("➕ Adicionar nova tarefa")
 with st.form("form_tarefa"):
-    id_tarefa = st.text_input("ID da tarefa (ex: T001)")
+    id_tarefa = st.text_input("ID Vantive")
     nome_tecnico = st.text_input("Nome do técnico")
     telefone = st.text_input("Telefone do técnico")
     descricao = st.text_area("Descrição da tarefa")
