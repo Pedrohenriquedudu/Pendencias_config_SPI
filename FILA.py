be576ada22582df1956a329c664896078f796353
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2025-10-16 19:21:35 -0300

    Update FILA.py

--- FILA.py
+++ FILA.py
@@ -112,12 +112,14 @@ with st.form("form_tarefa"):
 
     if enviar:
         if id and nome and telefone and descricao:
+            usuario_criador = st.session_state["usuario"]
             nova_tarefa = {
                 "id": id,
                 "nome": nome,
                 "telefone": telefone,
                 "descricao": descricao,
                 "status": "Pendente",
+                "criado_por": usuario_criador,
                 "data_criacao": (datetime.now() - timedelta(hours=3)).strftime("%d-%m-%Y %H:%M:%S"),
                 "data_assumido": "",
                 "data_encerrado": "",
@@ -126,7 +128,7 @@ with st.form("form_tarefa"):
             }
             tarefas.append(nova_tarefa)
             salvar_tarefas(tarefas)
-            st.success("✅ Tarefa adicionada com sucesso!")
+            st.success("✅ Tarefa adicionada com sucesso ! por {usuario_criador}")
             st.rerun()
         else:
             st.warning("⚠️ Preencha todos os campos antes de adicionar.")
